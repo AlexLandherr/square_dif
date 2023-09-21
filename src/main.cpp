ff6bfe40d8eed52a821ab5682facff17539dcf67
Alex Landherr <alex.landherr@gmail.com>
2023-09-21 09:13:53 +0000

    Init commit.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "include/functions.h"
+#include <iostream>
+
+/*
+The sum of the squares of the first ten natural numbers is: 1^2+2^2+...+10^2 = 385
+The square of the sum of the first ten natural numbers is:  (1+2+...+10)^2 = 3025
+And the difference between them is 3025 - 385 = 2640
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+int main() {
+
+    return 0;
+}
